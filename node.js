008ec53b8edc5cb3398118f32b98f7d66c08b59e
Yogesh Neupane <neupaneyogesh97@gmail.com>
2024-02-23 18:34:25 -0500

    link updated for cloud host

--- node.js
+++ node.js
@@ -12,7 +12,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 
-// Parse JSON bodies
+// parse JSON bodies
 app.use(express.json());
 
 app.use(bodyParser.json());
@@ -20,13 +20,13 @@ app.use(bodyParser.json());
 dotenv.config({ path: './config.env' });
 
 const InitiateMongoServer = require('./db.js');
-// Initialize Mongodb Connection
+// initialize Mongodb Connection
 InitiateMongoServer();
 
 const data=JSON.parse(fs.readFileSync('./recipes.json', 'utf-8'));
 
 
-app.use(cors()); // Enable CORS for all routes
+app.use(cors()); // enable CORS for all routes
 
 const mongoURI=process.env.MONGODB_URL;
 
@@ -34,7 +34,7 @@ mongoose.connect(mongoURI).then(()=>{
     importData();
 });
 
-// Importing data to MongoDB if the collection is empty
+// importing data to MongoDB if the collection is empty
 const importData = async()=>{
     try {
 
